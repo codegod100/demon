8bd81ef66e771bc2fa3801a54bf4eb1c24679861
vera <vera@debian.vera.pink>
2024-02-29 03:32:27 -0800

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Avatar } from "@skeletonlabs/skeleton";
+</script>
+
+<div class="pt-5 container mx-auto">
+  <div class="grid grid-flow-col auto-cols-max mb-5">
+    <Avatar
+      src="https://cdn.bsky.app/img/avatar/plain/did:plc:ngokl2gnmpbvuvrfckja3g7p/bafkreibpne7cdc42h3ueamxxvv2bk57etg7my655awtlbbde453cbj72me@jpeg"
+    />
+    <div class="align-middle"><span class="text-3xl">𝕯𝖊𝖒𝖔𝖓</span></div>
+  </div>
+  <div class="blockquote mb-5">
+    A borrowing of Latin daemōn (“tutelary deity”), from Ancient Greek δαίμων
+    (daímōn, “dispenser, tutelary deity”).
+  </div>
+  <div class="text-right mt-5">
+    <a href="https://en.wiktionary.org/wiki/daemon"
+      >https://en.wiktionary.org/wiki/daemon</a
+    >
+  </div>
+  <div class="h1 mb-5">My home on the 🕸️</div>
+  <div class="h2 mb-1">Social</div>
+  <div>
+    Mastodon: <a class="anchor" href="https://federate.social/@v"
+      >v@federate.social</a
+    >
+  </div>
+  <div>
+    Bluesky: <a class="anchor" href="https://bsky.app/profile/demon.ooo"
+      >@demon.ooo</a
+    >
+  </div>
+  <div class="h2 mt-5 mb-1">Why?</div>
+  <div>I bought a cool domain [demon.ooo] to use as bluesky account and decided to make a personal site using it.</div>
+</div>
